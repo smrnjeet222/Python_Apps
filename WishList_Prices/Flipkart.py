533b63a08afef2d6bb2ea09444ea3ea8ac670273
Simranjeet Singh <48654626+smrnjeet222@users.noreply.github.com>
2019-07-15 23:17:35 +0530

    Update Flipkart.py
    #Get Prices of your Wishlist items on the go.
    It prints the price of some of my desired products from Flipkart.

--- WishList_Prices/Flipkart.py
+++ WishList_Prices/Flipkart.py
@@ -18,7 +18,7 @@ print (f'The Price of pixel 3a is {pixel3a}')
 
 earphones = GetProductPrice("https://www.flipkart.com/boult-audio-bassbuds-oak-pure-wood-wired-headset-mic/p/itmff6zuhs466vca?pid=ACCFF6ZUBWXXDWZK&otracker=wishlist&lid=LSTACCFF6ZUBWXXDWZKMMQTES&fm=organic&iid=1d68a7f1-be67-4e73-b6a9-d37fbe61979e.ACCFF6ZUBWXXDWZK.PRODUCTSUMMARY&ppt=hp&ppn=hp")
 
-print (f'The price of boult earphones is {earphones}')
+print (f'The Price of boult earphones is {earphones}')
 
 sdcard = GetProductPrice("https://www.flipkart.com/sandisk-ultra-32-gb-microsdhc-class-10-98-mb-s-memory-card/p/itmeweghhhsdptty?pid=ACCEWEGHFTJ3G4ZG&otracker=wishlist&lid=LSTACCEWEGHFTJ3G4ZGCDXIVY&fm=organic&iid=7c6fc72e-0b57-464b-9fa4-c2b6d896cd88.ACCEWEGHFTJ3G4ZG.PRODUCTSUMMARY&ppt=hp&ppn=hp")
 
@@ -27,4 +27,6 @@ print(f'The Price of sd card is {sdcard}')
 UCBShoes = GetProductPrice("https://www.flipkart.com/united-colors-benetton-espadrilles-men/p/itmf3yyguydgkpqa?pid=SHOEV3TZTZXGC4SM&otracker=wishlist&lid=LSTSHOEV3TZTZXGC4SMGL7DEV&fm=organic&iid=4396fe3d-c0cd-4971-a13e-3fb36bfdb084.SHOEV3TZTZXGC4SM.PRODUCTSUMMARY&ppt=hp&ppn=hp")
 
 print(f'The Price of UCB espadrilles is {UCBShoes}')
+
+
 

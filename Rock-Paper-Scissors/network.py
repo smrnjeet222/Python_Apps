acb6c2138a7691f940aa4bdb619c51522c65bd71
Simranjeet Singh <smrnjeet222@gmail.com>
2020-04-22 12:46:43 +0530

    Rock Paper Scissors

---
+++ Rock-Paper-Scissors/network.py
@@ -0,0 +1,28 @@
+import socket
+import pickle
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = '192.168.31.85'
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as e:
+            print(e)
---
+++ Rock-Paper-Scissors/server.py
@@ -0,0 +1,78 @@
+from _thread import *
+import socket
+import pickle
+import sys
+from game import Game
+
+server = '192.168.31.85'
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen()
+
+print("Wating For Connection, Server Started")
+
+connected = set()
+games = {}
+idCount = 0
+
+
+def threaded_client(conn, p, gameid):
+    global idCount
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if gameid in games:
+                game = games[gameid]
+                
+                if not data:
+                    break
+                else:
+                    if data == 'reset':
+                        game.resetWent()
+                    elif data != 'get':
+                        game.play(p, data)
+
+                    reply = game
+                    conn.sendall(pickle.dumps(reply))
+            else:
+                break
+        except:
+            break
+
+    print("Lost Connection")
+    try:
+        del game[gameid]
+        print("Closing Game", gameid)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print('Connected to :', addr)
+
+    idCount += 1
+    p = 0
+    gameid = (idCount-1)//2
+
+    if idCount % 2 == 1:
+        games[gameid] = Game(gameid)
+        print('Creating a new game....')
+    else:
+        games[gameid].ready = True
+        p = 1
+
+    start_new_thread(threaded_client, (conn, p, gameid))

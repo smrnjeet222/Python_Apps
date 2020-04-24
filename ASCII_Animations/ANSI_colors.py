7c2a62da973210c0544d9befc605a60b52eb340e
Simranjeet Singh <smrnjeet222@gmail.com>
2020-04-24 15:18:59 +0530

    correction in colors

--- ASCII_Animations/ANSI_colors.py
+++ ASCII_Animations/ANSI_colors.py
@@ -3,11 +3,11 @@ import os
 os.system("cls")
 
 COLORS = {
-    "black": "\u001b[30;1m",
-    "red": "\u001b[31;1m",
+    "black": "\u001b[30m",
+    "red": "\u001b[31m",
     "green": "\u001b[32m",
-    "yellow": "\u001b[33;1m",
-    "blue": "\u001b[34;1m",
+    "yellow": "\u001b[33m",
+    "blue": "\u001b[34m",
     "magenta": "\u001b[35m",
     "cyan": "\u001b[36m",
     "white": "\u001b[37m",
